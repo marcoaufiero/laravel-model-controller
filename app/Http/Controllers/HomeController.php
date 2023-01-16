770e424b57cb667c9caa12c10f6a3b6d1fc71e66
marcoaufiero <marcoaufiero96@gmail.com>
2023-01-16 15:01:16 +0100

    HomeController added

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        
+        $all_movies = Movie::all();
+
+        dd($movies);
+
+        return view('welcome', compact('all_movies'));
+    }
+}
